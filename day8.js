8075ff4ea51905813ff3f8f01066f7b5dd9f127c
Lydia Shepherd <lydia.shepherd1@nhs.net>
2020-12-11 17:05:47 +0000

    day 8b

--- day8.js
+++ day8.js
@@ -21,6 +21,8 @@ function getInstructions(data) {
       return {
         nextMove: op === 'jmp' ? num : 1,
         acc: op === 'acc' ? num : 0,
+        op,
+        num,
       };
     });
 }
@@ -51,16 +53,40 @@ nop -4
 acc +6
 `;
 
-function day8b(data) {
-  const instructions = getInstructions(data);
-  let index = 0;
-  let acc = 0;
+function followInstructions(instructions, startAcc = 0, startIndex = 0) {
+  let acc = startAcc;
+  let index = startIndex;
+  let path = [];
   while (index < instructions.length && !instructions[index].visited) {
     instructions[index].visited = true;
     acc += instructions[index].acc;
+    if (instructions[index].acc === 0) {
+      path.push([index, acc]);
+    }
     index += instructions[index].nextMove;
   }
+  return index < instructions.length ? { path, acc } : { acc };
+}
+
+function day8b(data) {
+  const instructions = getInstructions(data);
+  const { acc, path } = followInstructions(instructions);
+
+  if(!path) {
+    return acc;
+  }
+
+  while (path.length) {
+    let [index, previousAcc] = path.pop();
+    index += instructions[index].op === 'jmp' ? 1 : instructions[index].num;
+    const { path: hasHitLoop, acc: newAcc } = followInstructions(instructions, previousAcc, index)
+    if(!hasHitLoop){
+      return newAcc
+    }
+  }
+
   return acc;
 }
 
 console.log(day8b(test2)); // 8
+console.log(day8b(test)); // 8
