7778eca194b7169542bcee974fd325c4194a0efe
Lydia Shepherd <lydia.shepherd1@nhs.net>
2020-12-14 13:05:30 +0000

    day 11b

--- day11.js
+++ day11.js
@@ -24,30 +24,53 @@ function getOccupiedNeighbours(rows, rowIndex, colIndex) {
   return occupiedNeighbours;
 }
 
-function getNewState(rows, rowIndex, colIndex) {
+function getOccupiedNeighboursBySightline(rows, rowIndex, colIndex) {
+  let isOccupied = rows[rowIndex][colIndex] === '#';
+  let occupiedNeighbours = isOccupied ? -1 : 0;
+  for (let r = -1; r <= 1; r++) {
+    for (let c = -1; c <= 1; c++) {
+      let distance = 1;
+      let neighbour, neighbourRow, neighbourCol;
+      do {
+        neighbourRow = rowIndex + distance * r;
+        neighbourCol = colIndex + distance * c;
+        neighbour = rows[neighbourRow] && rows[neighbourRow][neighbourCol];
+        distance += 1;
+      } while (neighbour && neighbour === '.' && (r || c));
+      if (neighbour === '#') {
+        occupiedNeighbours += 1;
+      }
+    }
+  }
+  return occupiedNeighbours;
+}
+
+function getNewState(rows, rowIndex, colIndex, maxNeighbours, bySight) {
   const currentState = rows[rowIndex][colIndex];
   if (currentState === '.') {
     return '.';
   }
-  const occupiedNeighbours = getOccupiedNeighbours(rows, rowIndex, colIndex);
+  const occupiedNeighbours = bySight
+    ? getOccupiedNeighboursBySightline(rows, rowIndex, colIndex)
+    : getOccupiedNeighbours(rows, rowIndex, colIndex);
   if (currentState === 'L' && occupiedNeighbours === 0) {
     return '#';
   }
-  if (currentState === '#' && occupiedNeighbours >= 4) {
+  if (currentState === '#' && occupiedNeighbours >= maxNeighbours) {
     return 'L';
   }
   return currentState;
 }
 
-function iterate(rows) {
+function iterate(rows, maxNeighbours = 4, bySight = false) {
   const newRows = [];
   let hasChanged = false;
   for (let r = 0; r <= rows.length - 1; r++) {
     let newRow = '';
     for (let c = 0; c <= rows[r].length - 1; c++) {
-      const newState = getNewState(rows, r, c);
+      const newState = getNewState(rows, r, c, maxNeighbours, bySight);
       newRow += newState;
-      if(!hasChanged && newState !== rows[r][c]){
+      if (!hasChanged && newState !== rows[r][c]) {
         hasChanged = true;
       }
     }
@@ -70,3 +93,14 @@ function day11a(data) {
 }
 
 console.log(day11a(test)); // 37
+
+function day11b(data) {
+  let rows = data.trim().split('\n');
+  let hasChanged = true;
+  while (hasChanged) {
+    [rows, hasChanged] = iterate(rows, 5, true);
+  }
+  return countOccupiedSeats(rows);
+}
+
+console.log(day11b(test)); // 26
